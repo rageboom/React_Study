d2f12bb34762a928dc8771fd725520aa4e6dd739
Ched <rageboom@gmail.com>
2016-07-21 11:15:39 +0900

    fixconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,18 @@
 module.exports = {
-    entry: './src/app.js',
+    entry: [
+        './src/app.js'
+    ],
     output: {
         path      : __dirname,
         filename  : './dist/bundle.js'
     },
     module: {
-        loader: [{
-            test  : /\.css$/,
-            loader: 'style!css'
-        },{
+        loaders: [{
             test   : /\.jsx?$/,
             loader : 'babel-loader',
             exclude: /node_modules/,
             query  : {
-                presets: ['es2015', 'react'],
-                plugins: ['transform-runtime']
+                presets: ['es2015', 'react']
             }
         }]
     }
